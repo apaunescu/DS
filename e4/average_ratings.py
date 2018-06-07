90fa143561d17a728915be7665ac5bd5d0cb0d3a
Alin <alinpaunescu@gmail.com>
2018-06-06 20:50:59 -0700

    Filter names/ratings and produce ratings for all titles

---
+++ e4/average_ratings.py
@@ -0,0 +1,39 @@
+import xml.etree.ElementTree as ET
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import difflib
+import sys
+
+
+textFile = sys.argv[1]
+csvFile = sys.argv[2]
+output = sys.argv[3]
+
+def cleaner(string):
+	string = string.rstrip('\n')
+	return string
+
+def realTitle(title):
+	match = difflib.get_close_matches(title, movies['title'], n=100, cutoff=0.6)
+	if (match == []):
+		match = "N/A"
+	return match[0]
+
+
+movieLegend = open(textFile, "r")
+titles = movieLegend.readlines()
+movies = pd.DataFrame(data = titles,
+	columns = ['title'])
+movies['title'] = movies['title'].apply(cleaner)
+userRatings = pd.read_csv(csvFile)
+
+
+userRatings['title'] = userRatings['title'].apply(realTitle)
+userRatings = userRatings.groupby('title').mean().reset_index()
+userRatings.rating = userRatings.rating.round(2)
+
+userRatings.to_csv(output)
+
+
+
